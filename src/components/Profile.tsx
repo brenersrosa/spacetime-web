9a766d0e68d8d5b81a1d2d7780ffc6b039bfaa4c
Brener Rosa <brenersrosa@gmail.com>
2023-05-18 18:11:49 -0300

    :fire: Save token in cookies and display profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { getUser } from '@/lib/auth'
+
+export function Profile() {
+  const { name, avatarUrl } = getUser()
+
+  return (
+    <div className="flex items-center gap-3 text-left">
+      <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+        <Image
+          src={avatarUrl}
+          alt="Profile image"
+          width={40}
+          height={40}
+          className="h-10 w-10 rounded-full"
+        />
+      </div>
+
+      <p className="max-w-[140px] text-sm leading-snug">
+        {name}
+        <Link
+          href=""
+          className="flex items-center transition-colors hover:text-red-300"
+        >
+          Quero sair
+        </Link>
+      </p>
+    </div>
+  )
+}
